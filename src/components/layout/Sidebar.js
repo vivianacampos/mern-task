f53ef7e383cd52a0ff5a8f2097580b0e072d1439
Viviana Campos <viviana.campos@analisischile.cl>
2022-04-04 17:55:54 -0400

    Sidebar

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <h1>
+        MERN<span>Task</span>
+      </h1>
+      <div className="proyectos">
+        <h2>Tus Proyectos</h2>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
